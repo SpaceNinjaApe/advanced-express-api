fac4f522cbb44b09c54ada513e86e0f670659832
Markus <28253325+SpaceNinjaApe@users.noreply.github.com>
2020-08-07 10:29:36 +0200

    Update request-description.decorator.ts
    FIX Typo
    MOD essential parameter check in one line

--- src/request-validator/request-description.decorator.ts
+++ src/request-validator/request-description.decorator.ts
@@ -3,19 +3,14 @@ import { RequestValidatorService } from './request-validator.service';
 
 /**
  * Decorator Factory for the request validation. If this object will be checked in the service a given, anonymous object will be type checked.
- * @param isAttributeEsentialsets Sets if this attribute should be set at all times. If so, a check will not just check the type but throw an error if undefined.
+ * @param isAttributeEssentialsets Sets if this attribute should be set at all times. If so, a check will not just check the type but throw an error if undefined.
  */
-export function requestAttribute (isAttributeEsential?: boolean): PropertyDecorator {
-    let essential: boolean;
+export function requestAttribute (isAttributeEssential?: boolean): PropertyDecorator {
 
-    if (isAttributeEsential === undefined) {
-        essential = false;
-    }  else {
-        essential = isAttributeEsential;
-    }
+    isAttributeEssential = (isAttributeEssential === undefined) ? false : isAttributeEssential;
     
     return function (target: Object, propertyKey: string | symbol) {
         let type: string = Reflect.getMetadata("design:type", target, propertyKey).name;
-        RequestValidatorService.getInstance().registerDescription(target.constructor.name, propertyKey as string, type.toLowerCase(), essential)
+        RequestValidatorService.getInstance().registerDescription(target.constructor.name, propertyKey as string, type.toLowerCase(), isAttributeEssential)
     }
 }
